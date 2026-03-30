86aa750f8c7a7389d1af5bf419c479e9dfdc99e3
Yuki Yoshinaga <yuki-yoshinaga@2y-systems.com>
2026-03-30 16:04:00 +0900

    Watchの最初の画面をなくした

--- SkippingRope Watch App/ContentView.swift
+++ SkippingRope Watch App/ContentView.swift
@@ -3,14 +3,7 @@ import SwiftUI
 struct ContentView: View {
     var body: some View {
         NavigationStack {
-            List {
-                NavigationLink {
-                    WatchRecordView()
-                } label: {
-                    Label("ジャンプ記録", systemImage: "figure.jumprope")
-                }
-            }
-            .navigationTitle("なわとび")
+            WatchRecordView()
         }
     }
 }
