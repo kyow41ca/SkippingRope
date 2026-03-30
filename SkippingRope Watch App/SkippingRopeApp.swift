1973b99a33b94429c4fe1ca66b118608d917e649
Yuki Yoshinaga <yuki-yoshinaga@2y-systems.com>
2026-03-30 15:05:51 +0900

    Initial Commit

---
+++ SkippingRope Watch App/SkippingRopeApp.swift
@@ -0,0 +1,17 @@
+//
+//  SkippingRopeApp.swift
+//  SkippingRope Watch App
+//
+//  Created by Yuki Yoshinaga on 2026/03/30.
+//
+
+import SwiftUI
+
+@main
+struct SkippingRope_Watch_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ SkippingRope/SkippingRopeApp.swift
@@ -0,0 +1,17 @@
+//
+//  SkippingRopeApp.swift
+//  SkippingRope
+//
+//  Created by Yuki Yoshinaga on 2026/03/30.
+//
+
+import SwiftUI
+
+@main
+struct SkippingRopeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
